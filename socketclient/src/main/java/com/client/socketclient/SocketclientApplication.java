d120c482afba88be20269502d37bdfad25da42fd
zhangjiechao <zhangjiechao@codemao.cn>
2019-10-14 10:45:18 +0800

    start

--- socketclient/src/main/java/com/client/socketclient/SocketclientApplication.java
+++ socketclient/src/main/java/com/client/socketclient/SocketclientApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.client.socketclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SocketclientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SocketclientApplication.class, args);
     }
 
 }
